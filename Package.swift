5728230b2b0deed071fdd68ca3017cf8715db5f7
jpalathu <39676137+jpalathu@users.noreply.github.com>
2022-08-24 12:36:17 -0400

    Revert "Update Package.swift"

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
  let package = Package(
      name: "BlueSwift",
-     platforms: [ .iOS(.v15) ],
+     platforms: [ .iOS(.v10) ],
      products: [
          .library(
              name: "BlueSwift",
